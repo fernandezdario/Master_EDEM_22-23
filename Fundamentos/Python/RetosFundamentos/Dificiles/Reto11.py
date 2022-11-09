ba471679cb472f2619916efcc78ab3b40e38de4e
darydaro <100683177+fernandezdario@users.noreply.github.com>
2022-11-10 00:22:05 +0100

    code

--- Fundamentos/Python/RetosFundamentos/Dificiles/Reto11.py
+++ Fundamentos/Python/RetosFundamentos/Dificiles/Reto11.py
@@ -4,38 +4,54 @@ El programa debe mostrar las siguientes opciones para que escoja el usuario:
 (1) Añadir un cliente
 (2) Eliminar cliente por NIF
 (3) Mostrar Cliente por NIF
-(4) Listar TODOS os clientes
+(4) Listar TODOS los clientes
 (5) Mostrar ÚNICAMENTE los clientes preferentes
 (6) Finalizar Programa'''
-
 import csv
-def cartera_cientes():
-    cliente = {}
+clientes = {}
 
+def cartera_cientes():
     print('Bienvenido al sistema! A continuación, apareceran una serie de comandos que puedes accionar.')
     eleccion = int(input('(1) Añadir un cliente \n(2) Eliminar cliente por NIF \n(3) Mostrar Cliente por NIF \n(4) Listar TODOS os clientes \n(5) Mostrar ÚNICAMENTE los clientes preferentes \n(6) Finalizar Programa \nAcción escogida: '))
 
     if eleccion == 1:
+        # creamos los valores del diccionario, de forma interactiva, mediante consola
         nif = str(input('Escribe aquí tu NIF: '))
         nombre = str(input('Escribe aquí tu nombre: '))
         apellidos = str(input('Escribe aquí tus apellidos: '))
         telefono = str(input('Escribe aquí tu teléfono: '))
         email = str(input('Escribe aquí tu email: '))
         preferente = bool(input('¿Tiene un contrato preferente? En caso de que así sea escriba True, en caso contrario False: '))
-        cliente = {'NIF': nif, 'Nombre': nombre, 'Apellidos': apellidos, 'Teléfono': telefono, 'Email': email, 'Preferente': preferente}
-        with open('clientes.csv', 'a') as g:  
+        #aquí le damos a los valores creados previamente unas claves para que se puedan alamacenar en el diccionario
+        cliente = {'NIF': nif, 'Nombre': nombre, 'Apellidos': apellidos, 'Telefono': telefono, 'Email': email, 'Preferente': preferente}
+        clientes[nif] = cliente
+        #el programa guarda el diccionario en un csv y almacena los datos en formato clave, valor. En este caso la clave será el nif, mientras que el valor será el resto del diccionario.
+        with open('C:\\Users\\dario\\OneDrive\\Documentos\\GitHub\\M-ster-EDEM-22-23\\Fundamentos\\Python\\RetosFundamentos\\Dificiles\\clientes.csv', 'a') as g:  
             a = csv.writer(g)
-            for k, v in cliente.items():
+            for k, v in clientes.items():
                 a.writerow([k, v])
-    elif eleccion == 2:
+    if eleccion == 2:
+        print('Has seleccionado la opción de eliminar un cliente por NIF.')
+        nif = str(input('Escribe el NIF del cliente que quieras eliminar: '))
+        if nif in clientes:
+            del cliente
+            with open('C:\\Users\\dario\\OneDrive\\Documentos\\GitHub\\M-ster-EDEM-22-23\\Fundamentos\\Python\\RetosFundamentos\\Dificiles\\clientes.csv', 'w') as g:  
+                writer = csv.writer(g)
+                for k, v in clientes.items():
+                    writer.writerow([k, v])
+        else:
+            print('No existe este NIF.')
+    if eleccion == 3:
+        nif = str(input('Escribe el NIF del cliente que quieras encontrar: '))
+        if nif in clientes:
+            print(cliente)
+        else:
+            print('Este NIF no existe.')
+    if eleccion == 4:
+        print(clientes)
+    if eleccion == 5:
         'sadfsdfsfd'
-    elif eleccion == 3:
-        'sadfsdfsfd'
-    elif eleccion == 4:
-        'sadfsdfsfd'
-    elif eleccion == 5:
-        'sadfsdfsfd'
-    elif eleccion == 6:
+    if eleccion == 6:
         'sadfsdfsfd'
     else:
         print('Solamente existen comandos del 1 al 6, por favor elija un número comprendido en ese rango.')
