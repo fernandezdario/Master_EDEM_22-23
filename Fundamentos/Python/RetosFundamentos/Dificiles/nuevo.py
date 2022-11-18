28412122bae984cecede325572d14369fe33279b
darydaro <100683177+fernandezdario@users.noreply.github.com>
2022-11-18 16:12:29 +0100

    Reto11

--- Fundamentos/Python/RetosFundamentos/Dificiles/nuevo.py
+++ Fundamentos/Python/RetosFundamentos/Dificiles/nuevo.py
@@ -9,11 +9,9 @@ El programa debe mostrar las siguientes opciones para que escoja el usuario:
 (6) Finalizar Programa'''
 
 from Clientes import Clientes
-cliente1 = Clientes('45464148J', 'Joaquin', 'Reyes Gomez', '694564123','joarego@gmail.com', preferente = False)
-cliente2 = Clientes('45464348D', 'Fernando', 'Sebastian Lopez', '642789453','nandose@gmail.com', preferente = True)
-cliente3 = Clientes('35428342D', 'Paco', 'Beltran Torres', '621248795','pabeto@gmail.com', preferente = True)
-cliente4 = Clientes('66666666F', 'Pablo', 'Gonzalez Martinez', '654987321', 'pagonza@edem.es', preferente = False)
-cartera_clientes = [cliente1, cliente2, cliente3, cliente4]
+cliente1 = Clientes('85082725C', 'Nieves', 'Fernandez Castillo', '699487511','nfer@gmail.com', preferente = False)
+cliente2 = Clientes('24334318L', 'Heraclio', 'Fernandez Mejias', '629687591','vief@gmail.com', preferente = True)
+cartera_clientes = [cliente1, cliente2]
 eleccion = 0
 print('Bienvenido a la pantalla de seleccion de la carte de clientes!')
 while eleccion != 6:
@@ -28,24 +26,40 @@ while eleccion != 6:
         email = str(input('Escribe aquí tu email: '))
         preferente = bool(input('¿Tiene un contrato preferente? En caso de que así sea escriba True, en caso contrario False: '))
         #aquí le damos a los valores creados previamente unas claves para que se puedan alamacenar en el diccionario
-        cliente = Clientes(NIF, nombre, apellido, telefono, email, preferente)
-        cartera_clientes.append(cliente)
+        cliente_nuevo = Clientes(NIF, nombre, apellido, telefono, email, preferente)
+        cartera_clientes.append(cliente_nuevo)
     elif eleccion == 2:
-
-        print(f"\nHas seleccionado ({eleccion}): Has seleccionado eliminar un cliente por NIF")
-  
-        NIF = str(input("Introduzca el NIF: "))
-
-        for n in Clientes:
-            if n.NIF == NIF:
-                Clientes.remove(n)
-
-# Si introducimos 3: Mostramos un cliente de la carteta según el NIF
+        print('Has seleccionado la opción de eliminar un cliente por NIF.')
+        NIF = str(input('Escribe el NIF del cliente que quieras eliminar: '))
+        for cliente in cartera_clientes:
+            if cliente.NIF == NIF:
+                cartera_clientes.remove(cliente)
+                print('El cliente ha sido eliminado, de manera exitosa!')
+            else:
+                print('No existe este NIF.')
+                break
     elif eleccion == 3:
+        print('Has seleccionado la opción de listar un cliente por NIF.')
+        NIF = str(input('Escribe el NIF del cliente que quieras encontrar: '))
+        for cliente in cartera_clientes:
+            if cliente.NIF == NIF:
+                print(f'\n\nNIF: {NIF}\nNombre: {cliente.nombre}\nApellidos: {cliente.apellido}\nTelefono: {cliente.telefono}\nEmail:{cliente.email}\nPreferente: {cliente.preferente}')
+            else:
+                print('Este NIF no existe.')
+                break
+    elif eleccion == 4:
+        print('Has seleccionado la opción de mostrar todos los clientes.')
+        for cliente in cartera_clientes:
+            print(f'\n\nNIF: {cliente.NIF}\nNombre: {cliente.nombre}\nApellidos: {cliente.apellido}\nTelefono: {cliente.telefono}\nEmail:{cliente.email}\nPreferente: {cliente.preferente}')
+    elif eleccion == 5:
+        print('Has seleccionado la opción de mostrar todos los clientes preferentes.')
+        for cliente in cartera_clientes:
+            if cliente.preferente == True:
+                print(f'\n\nNIF: {cliente.NIF}\nNombre: {cliente.nombre}\nApellidos: {cliente.apellido}\nTelefono: {cliente.telefono}\nEmail:{cliente.email}\nPreferente: {cliente.preferente}')
+    elif eleccion == 6:
+        break
+    else:
+        print('Este programa solo contempla acciones del 1 al 6, por favor vuelva a introducir una acción que se contemple en los parámetros del programa.')
+        eleccion = int(input('(1) Añadir un cliente \n(2) Eliminar cliente por NIF \n(3) Mostrar Cliente por NIF \n(4) Listar TODOS os clientes \n(5) Mostrar ÚNICAMENTE los clientes preferentes \n(6) Finalizar Programa \nAcción escogida: '))
 
-        print(f"\nHas seleccionado ({eleccion}): Has seleccionado mostrar un cliente por NIF")
 
-        NIF = str(input("Introduzca el NIF: "))
-    for n in Clientes:
-      if n.NIF == NIF:
-         print(f'\n\nNIF: {n.NIF}\nNombre: {n.nombre}\nApellidos: {n.apellidos}\nTelefono: {n.telefono}\nEmail:{n.email}\nPreferente: {n.preferente} ')
